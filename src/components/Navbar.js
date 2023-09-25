04eff3e56d6bcd4de3245ae0570889b34387b267
Anjali Panchariya <anjalipanchariya11@gmail.com>
2023-09-25 22:20:29 +0530

    bills commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,29 @@
 import React from "react";
 import '../styles/Navbar.css'
 import logo from '../images/logo.jfif'
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import Bills from './Bills'
+import { BrowserRouter as Router, Link, Routes, Route} from "react-router-dom";
 function Navbar(){
   return(
     <>
     <Router>
     <div className="navbar">
       <ul>
-
-        <li><a href="">
-          <img src={logo} alt="" /></a></li>
-        <li><a href="">Notes</a></li>
-        <li><a href="">Incomes</a></li>
-        <li><a href="">Expenses</a></li>
-        <li><a href="">Graphs</a></li>
+        <li><Link to="/"><img src={logo} alt="" /></Link></li>
+        <li><Link to="/bills">Bills</Link></li>
+        <li><Link to="/incomes">Incomes</Link></li>
+        <li><Link to="/expenses">Expenses</Link></li>
+        <li><Link to="/graphs">Graphs</Link></li>
       </ul>
+      <div className="lg">
+
+      </div>
+      <Routes>
+        <Route exact path="/bills" element={<Bills/>}></Route>
+        {/* <Route exact path="/incomes" element = {<Incomes/>}></Route>
+        <Route exact path="/expenses" element = {<Expense/>}></Route>
+        <Route exact path="/graphs" element = {<Graphs/>}></Route> */}
+      </Routes>
     </div>
     </Router>
     </>
